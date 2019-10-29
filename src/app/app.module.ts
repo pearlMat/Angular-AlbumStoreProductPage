8a27d2d893f4577e596ad79dcc51cffb9378dd71
Pearl Takang <pearllyt@gmail.com>
2019-10-29 07:52:29 +0100

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 //import { ProductDescriptionComponent } from './product-description/product-description.component';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
     ProductPageComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
+    ProductListComponent,
   //  ProductDescriptionComponent
   ],
   imports: [
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import  {Http, Response} from '@angular/http';
 
 import  'rxjs/add/operator/map';
 import{Album} from './album';
+import{Product}from './product';
 import {Observable} from 'rxjs/Observable';
 
 
@@ -12,6 +13,7 @@ import {Observable} from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl ='../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -19,6 +21,13 @@ export class ProductService {
 
   	return this._http.get(this._albumUrl).map((response) =>
   	<Album>response.json());
+
+  }
+
+   getProducts(): Observable<Product[]>{
+
+  	return this._http.get(this._productsUrl).map((response) =>
+  	<Product[]>response.json());
 
   }
 
